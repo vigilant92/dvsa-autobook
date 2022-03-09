7f69f0a070ba7722c43a4fcfd264c4e9c96d3c3b
Tanvir Samad <visilant@gmail.com>
2022-03-09 13:23:13 +0000

    Add project files.

---
+++ test_train/Booking.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace test_train
+{
+    partial class Booking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(185, 8);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1087, 674);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 179);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(89, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "License:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 57);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Reference:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 57);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(89, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(70, 84);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(89, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 86);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Search For:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 114);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Month/Year";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(70, 112);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(89, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Text = "07/2022";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(70, 140);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(89, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 142);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Avoid For";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(5, 231);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.MaxLength = 327678888;
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(169, 452);
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(93, 179);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(81, 20);
+            this.textBox7.TabIndex = 12;
+            this.textBox7.Text = "15000";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // Booking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 690);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Booking";
+            this.Text = "test";
+            this.Load += new System.EventHandler(this.test_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
---
+++ test_train/Booking.cs
@@ -0,0 +1,501 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_train
+{
+    public partial class Booking : Form
+    {
+        public Booking()
+        {
+            InitializeComponent();
+        }
+       
+        public CefSharp.WinForms.ChromiumWebBrowser br;
+
+        private void loadall()
+        {
+            string reffer = "";
+            count = 2;
+            string postData = "javascriptEnabled=true&_csrf=" + _csrf + "&passwordType=NORMAL&username=" + _license + "&password=" + _reference + "&alternativePassword=&booking-login=Continue";
+            //   PostData("https://driverpracticaltest.dvsa.gov.uk/login", PostData);
+            link = "https://driverpracticaltest.dvsa.gov.uk/login";
+            byte[] bytes = Encoding.ASCII.GetBytes(postData);
+            //  string x = await Post(link, bytes, _browser);
+            //LoadPost(link, postData);
+
+
+            Thread.Sleep(5000);
+            //edit date
+            count = 3;
+            link = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial + "&_eventId=editTestDateTime";
+            reffer = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial;
+            _executionSerial = "";
+
+           
+         //   br.(link,reffer)
+            br.Load(link);
+
+
+
+            Thread.Sleep(5000);
+            while (!(_executionSerial.Length > 1))
+            {
+                Thread.Sleep(200);
+
+            }
+
+            //change date
+            count = 4;
+                            link = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial + "&_eventId=proceed";
+            reffer = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial;
+            _executionSerial = "";
+                         postData = "_csrf=" + _csrf + "&testChoice=DATE&preferredTestDate=09/04/22&drivingLicenceSubmit=Continue";
+                        bytes = Encoding.ASCII.GetBytes(postData);
+                        LoadPost(link, postData, reffer);
+
+          //  LoadGet(link, reffer);
+            Thread.Sleep(5000);
+
+            while (!(_executionSerial.Length > 1))
+            {
+                Thread.Sleep(200);
+
+            }
+
+            //change center
+            count = 5;
+                         link = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial + "&_eventId=changeTestCentre";
+            reffer = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial;
+            _executionSerial = "";
+                         br.Load(link);
+           
+
+            Thread.Sleep(5000);
+            while (!(_executionSerial.Length > 1))
+            {
+                Thread.Sleep(200);
+
+            }
+
+            //search center
+            count = 6;
+                        Random rnd = new Random();
+                        while (true)
+                        {
+                            link = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial + "&_eventId=search";
+                              reffer = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial;
+                            _executionSerial = "";
+                             postData = "_csrf=" + _csrf + "&testCentreName=" + _testCenter + "&testCentreSubmit=Find+test+centres";
+
+                            bytes = Encoding.ASCII.GetBytes(postData);
+                                 LoadPost(link, postData, reffer);
+              
+                          
+
+                           
+
+                            if (_centerId.Length > 1)
+                            { count = 7;
+                                break;
+                            }
+
+                            Thread.Sleep(5000 + rnd.Next(0, Convert.ToInt32(textBox7.Text)));
+
+                        }
+           // Thread.Sleep(2000);
+            while (!(_executionSerial.Length > 1))
+            {
+                Thread.Sleep(200);
+
+            }
+
+            // in center find slot
+            link = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial + "&_eventId=proceed&centreID=" + _centerId;
+            reffer = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial;
+            _executionSerial = "";
+                         br.Load(link);
+
+
+            //      Thread.Sleep(2000);
+
+            while (!(_executionSerial.Length > 1))
+            {
+                Thread.Sleep(200);
+
+            }
+
+            while (true)
+            {
+
+                if (_slot.Length > 1)
+                {
+                    count = 8;
+
+
+
+
+                    link = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial + "&_eventId=proceed";
+                    reffer = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial;
+                    _executionSerial = "";
+                    postData = "_csrf=" + _csrf + "&slotWarningAcknowledged=true&slotTime=" + _slot;
+                    bytes = Encoding.ASCII.GetBytes(postData);
+                    LoadPost(link, postData, reffer);
+                  
+
+                    Thread.Sleep(2000);
+
+                    while (!(_executionSerial.Length > 1))
+                    {
+                        Thread.Sleep(200);
+
+                    }
+
+                    count = 9;
+                    link = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial + "&_eventId=candidate";
+                    reffer = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial;
+                    _executionSerial = "";
+                    br.Load(link);
+                  
+
+              //      Thread.Sleep(2000);
+                    //    _slot = findslot(Text, "July 2022");
+                    while (!(_executionSerial.Length > 1))
+                    {
+                        Thread.Sleep(200);
+
+                    }
+                    // return;
+                    Debug.WriteLine("Came Here");
+                    //confirm
+            
+                            count = 10;
+                    link = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial + "&_eventId=proceed";
+                    reffer = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial;
+                    _executionSerial = "";
+                    postData = "_csrf=" + _csrf + "&isConfirm=true";
+                    bytes = Encoding.ASCII.GetBytes(postData);
+                    //  link = "https://driverpracticaltest.dvsa.gov.uk/manage?execution=" + _executionSerial + "&_eventId=abandon";
+                     LoadPost(link, postData, reffer);
+                  //  WriteLog(x.ToString(), "10th page", 1010);
+
+                }
+                Thread.Sleep(1000);
+
+            }
+
+
+
+        }
+
+
+        string findcenter(string text, string subdate, string nocenter)
+        {
+            if (!text.Contains(subdate))
+            {
+                return "";
+
+            }
+
+            text = text.Substring(text.IndexOf(subdate) - 450, 450);
+            if (text.Contains(nocenter))
+            {
+                return "";
+            }
+            string first = "centreID=";
+            string last = "\" id";
+            string x = text.Substring(text.IndexOf(first) + first.Length);
+            string output = x.Substring(0, x.IndexOf(last));
+
+            return output;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _license = textBox1.Text;
+            _reference = textBox2.Text;
+            _testCenter = textBox3.Text;
+             subdate = textBox4.Text;
+            nocenter = textBox5.Text;
+            DateTime dt = DateTime.ParseExact("01" + subdate, "dd/MM/yyyy", null);
+            submonthyear = dt.ToString("MMMM") + " " + dt.ToString("yyyy");
+
+
+            Thread tr = new Thread(loadall);
+            tr.Start();
+
+     
+        }
+        string _license = "";
+        string _reference = "";
+        string _executionSerial = "";
+        string _centerId = "";
+        string _slot = "";
+        string _testCenter = "";
+        string subdate = "";
+        string submonthyear = "";
+        string nocenter = "";
+        string link = "";
+        int count = 0;
+        string _csrf = "";
+        string currentUrl = "";
+
+        public void LoadPost(string url, string postData, string referer)
+        {
+            var frame = br.GetMainFrame();
+
+            //Create a new request knowing we'd like to use PostData
+            var request = frame.CreateRequest(initializePostData: true);
+            request.Method = "POST";
+            request.Url = url;
+            //Set AllowStoredCredentials so cookies are sent with Request
+            request.Flags = UrlRequestFlags.AllowStoredCredentials;
+            request.PostData.AddData(postData);
+            request.Headers.Add("Referer", referer);
+            frame.LoadRequest(request);
+        }
+      
+        private void test_Load(object sender, EventArgs e)
+        {
+        
+           
+            loadbr();
+
+           //
+            string lnk = "https://driverpracticaltest.dvsa.gov.uk/login";
+
+            br.Load(lnk);
+
+            
+        }
+        void loadbr()
+        {
+            br = new CefSharp.WinForms.ChromiumWebBrowser()
+            {
+
+                Dock = DockStyle.Fill,
+                BrowserSettings = new BrowserSettings()
+
+     
+
+
+            };
+           
+            br.BrowserSettings.ImageLoading = CefState.Enabled;
+            br.BrowserSettings.Javascript = CefState.Enabled;
+         
+
+            br.AddressChanged += Browser_AddressChanged;
+            br.FrameLoadEnd += Browser_FrameLoadEnd;
+          
+            this.panel1.Controls.Add(br);
+
+        }
+        private void Browser_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            currentUrl = e.Address;
+           
+        }
+        async void Browser_FrameLoadEnd(object sender, CefSharp.FrameLoadEndEventArgs e)
+        {
+            //  Debug.WriteLine("cef-" + e.Url);
+
+            //   if (e.Frame.IsMain)
+            //   {
+            string test = await e.Frame.GetSourceAsync();
+            if (_csrf.Length > 1)
+            {
+                Debug.WriteLine(test);
+            }
+            else
+            {
+                _csrf = findcsrf(test);
+                if (_csrf.Length > 1)
+                {
+                    WriteLog(test.ToString(), "1st page", 1010);
+
+                    if (textBox6.InvokeRequired)
+
+                    {
+
+                        textBox6.Invoke((MethodInvoker)delegate { textBox6.Text = textBox6.Text + "Ready To Start :" + _csrf + Environment.NewLine; });
+
+                    }
+               
+                }
+
+            }
+
+            
+                if (_executionSerial.Length > 1)
+                {
+
+                }
+                else
+                {
+                    _executionSerial = findit(test, "execution=", "&amp;");
+                    if (_executionSerial.Length > 1)
+                    {
+                        WriteLog(test.ToString(), count + " page", 1010);
+                        if (textBox6.InvokeRequired)
+
+                        {
+                        
+                            textBox6.Invoke((MethodInvoker)delegate { textBox6.Text = textBox6.Text + "Logged In :" + _executionSerial + Environment.NewLine; });
+
+                        }
+                      
+                    }
+                }
+            
+
+            if (count == 6)
+            {
+
+               
+
+                if (_centerId.Length > 1)
+                {
+
+                }
+                else
+                {
+                    _centerId = findcenter(test, subdate, nocenter);
+                    if (_centerId.Length > 1)
+                    {
+                        WriteLog(test.ToString(), count + " page", 1010);
+                        if (textBox6.InvokeRequired)
+
+                        {
+                            
+                            textBox6.Invoke((MethodInvoker)delegate { textBox6.Text = textBox6.Text + "Got Center  :" + _centerId + " Logged In :" + _executionSerial +  Environment.NewLine; });
+
+                        }
+                        
+                    }
+                }
+              
+
+            }
+            if(count == 7)
+            {
+              
+
+                if (_slot.Length > 1)
+                {
+
+                }
+                else
+                {
+                    _slot = findslot(test, submonthyear);
+                    if (_slot.Length > 1)
+                    {
+                        WriteLog(test.ToString(), count + " page", 1010);
+                        if (textBox6.InvokeRequired)
+
+                        {
+                            //   count = 3;
+                            textBox6.Invoke((MethodInvoker)delegate { textBox6.Text = textBox6.Text + "Got Slot  :" + _slot + " Got Center  :" + _centerId + " Logged In :" + _executionSerial + Environment.NewLine; });
+
+                        }
+                      
+                    }
+                }
+            }
+           
+        }
+
+        string findslot(string text, string subdate)
+        {
+            if (!text.Contains(subdate))
+            {
+                return "";
+
+            }
+
+            text = text.Substring(text.IndexOf(subdate) + subdate.Length, 200);
+
+            string first = "\" for=\"slot-";
+            string last = "\">";
+            string x = text.Substring(text.IndexOf(first) + first.Length);
+            string output = x.Substring(0, x.IndexOf(last));
+
+            return output;
+
+        }
+        string findcsrf(string text)
+        {
+            if (!text.Contains("_csrf\" value=\""))
+            {
+                return "";
+            }
+            string x = text.Substring(text.IndexOf("_csrf\" value=\"") + "_csrf\" value=\"".Length);
+            string csrf = x.Substring(0, x.IndexOf("\">"));
+
+            return csrf;
+
+        }
+        string findit(string text, string first, string last)
+        {
+            if (!text.Contains(first))
+            {
+                return "";
+            }
+            string x = text.Substring(text.IndexOf(first) + first.Length);
+            string output = x.Substring(0, x.IndexOf(last));
+
+            return output;
+
+        }
+
+        public static void WriteLog(string strLog, string name, int i)
+        {
+            try
+            {
+                if (name.Length > 0)
+                {
+                    name = name + "-";
+                }
+                string logFilePath = System.IO.Path.GetDirectoryName(Application.ExecutablePath).ToString() + "\\Logs\\Log-" + name.ToString() + i.ToString() + "_" + DateTime.Now.ToString("yyyyMMddTHHmmssfff").ToString() + "." + "txt";
+               
+                FileInfo logFileInfo = new FileInfo(logFilePath);
+                DirectoryInfo logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
+                if (!logDirInfo.Exists) logDirInfo.Create();
+                using (FileStream fileStream = new FileStream(logFilePath, FileMode.Append))
+                {
+                    using (StreamWriter log = new StreamWriter(fileStream))
+                    {
+                        log.WriteLine(DateTime.Now.ToString() + Environment.NewLine + Environment.NewLine + strLog);
+                    }
+                }
+            }
+            catch (Exception x)
+            {
+
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string lnk = "https://driverpracticaltest.dvsa.gov.uk/login";
+
+            br.Load(lnk);
+        }
+    }
+}
